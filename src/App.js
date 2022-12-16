969c95854ee84779a0ade82e366689903a416bab
Ahmad Mustafa <112854816+Ahmadkodehode@users.noreply.github.com>
2022-12-16 12:18:39 +0100

    Tailwindcss

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import React, { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <h1>
+    <div className="App flex">
+      <h1 className="text-3xl font-bold ">
         The Counter Is<span className="mySpan"> {count}</span>
       </h1>
       <div className="btn">
-        <button onClick={() => setCount(0)}>Reset</button>
+        <button className="text-3xl font-bold " onClick={() => setCount(0)}>
+          Reset
+        </button>
         <button onClick={() => setCount(count + 1)}> +1 </button>
         <button onClick={() => setCount(count - 1)}>-1</button>
         <button onClick={() => setCount(count * 10)}>* 10</button>
